00413ec255aeda94a71082665a27423939d576aa
Oscar Romero <oscar.romero@desilico.tech>
2019-10-23 22:09:40 +0200

    clean create-react-app, add folders and fetch json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let url = "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json";
+
+  fetch(url)
+  .then(res => res.json())
+  .then((out) => {
+    console.log('Checkout this JSON! ', out);
+  })
+  .catch(err => { throw err });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hi, I'm a React App!</h1>
     </div>
   );
 }
